a6eac5e990bd6c05c7850ab9fcc7aa9b462483dd
Hobinx <binservmail@gmail.com>
2017-06-07 22:34:21 +0800

    Initial commit

---
+++ lib/ex_issues.ex
@@ -0,0 +1,18 @@
+defmodule ExIssues do
+  @moduledoc """
+  Documentation for ExIssues.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExIssues.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_issues_test.exs
@@ -0,0 +1,8 @@
+defmodule ExIssuesTest do
+  use ExUnit.Case
+  doctest ExIssues
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
