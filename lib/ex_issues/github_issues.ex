372830aef86de28809559d4c63ac8e121b3bfb05
Hobinx <binservmail@gmail.com>
2017-06-08 21:49:24 +0800

    use poison parse json

--- lib/ex_issues/github_issues.ex
+++ lib/ex_issues/github_issues.ex
@@ -5,10 +5,12 @@ defmodule ExIssues.GithubIssues do
     issues_url(user, repo)
     |> HTTPoison.get(@user_agent)
     |> handle_response
+    |> parse_json
   end
 
+  @github_url Application.get_env(:ex_issues, :github_url)
   def issues_url(user, repo) do
-    "https://api.github.com/search/issues?q=user:#{user}+repo:#{repo}"
+    "#{@github_url}?q=user:#{user}+repo:#{repo}"
   end
 
   def handle_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
@@ -20,6 +22,14 @@ defmodule ExIssues.GithubIssues do
   end
 
   def handle_response({:error, %HTTPoison.Error{reason: reason}}) do
+    {:error, {"message", reason}}
+  end
+
+  def parse_json({:ok, body}) do
+    {:ok, Poison.Parser.parse!(body)}
+  end
+
+  def parse_json({:error, reason}) do
     {:error, reason}
   end
 end
